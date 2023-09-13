201b4ad8449461ded9d49efb78dbaaae5897499c
Lucas Flor Vilela <lucasvilelapessoal@gmail.com>
2023-09-13 11:10:05 -0300

    Update MainTest.java

--- codigo/Trab01/src/test/MainTest.java
+++ codigo/Trab01/src/test/MainTest.java
@@ -7,7 +7,13 @@ public class MainTest {
 	@Test
 	public void test() {
 		
+		Aluguel a = new Aluguel();
+		Equipamento Equip1 = new Equipamento)();
+		Equip1.setIdEquip(01);
+		Equip1.setDescricao("Pá de ferro feita para cavar");
 		
+		.assertEquals(10.00, a.setValorDiario(10.00));
+		.assertEquals(Equip1.IdEquip, a.equipamento.IdEquip)
 		
 	}
 
